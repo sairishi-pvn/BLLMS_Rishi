b1d72b64030f171c6ccfce920400a6deb0f00e47
Sai Rishi P V N <sairishi.pvn@gmail.com>
2026-07-22 13:07:57 +0530

    text extracted, and tokenized...seperated well

--- ch01/the_verdict_text_extraction.py
+++ ch01/the_verdict_text_extraction.py
@@ -1,9 +1,13 @@
 import urllib.request
+import re
 url = ("https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt")
 file_path  = "the-verdict.txt"
 urllib.request.urlretrieve(url, file_path)
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
-print("Total number of characters:", len(raw_text))
-print(raw_text[:99])  
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(len(preprocessed))
+print(preprocessed[:30])
