2b6aa782d590b558fb7b633b620b7bd6882a12ec
Sai Rishi P V N <sairishi.pvn@gmail.com>
2026-07-22 12:12:44 +0530

    loaded verdict text and read

--- ch01/the_verdict_text_extraction.py
+++ ch01/the_verdict_text_extraction.py
@@ -2,3 +2,8 @@ import urllib.request
 url = ("https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt")
 file_path  = "the-verdict.txt"
 urllib.request.urlretrieve(url, file_path)
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+print("Total number of characters:", len(raw_text))
+print(raw_text[:99])  
